3f2353e332274681125a420538991d3a3bbfdd4d
Leothurm <leonardo.thurm@gmail.com>
2023-01-31 19:38:05 -0300

    JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let interval;
+
+
 function start(){
-    console.log('iniciou')
+    count()
+   interval = setInterval(count, 1000)
 }
 function pause(){
-    console.log('pausou')
+    clearInterval(interval)
 }
 function stop(){
-    console.log('parou')
+    clearInterval(interval)
+    seconds = 0;
+    minutes = 0;
+    document.getElementById('timer').innerHTML = '00:00:00'
+}
+
+function showZero(digit){
+    if(digit<10){
+        return '0' + digit
+    }else{
+        return digit 
+    }
+}
+
+function count(){
+    seconds++
+    if(seconds == 60){
+        minutes++
+        seconds = 0
+    }
+
+    if(minutes == 60){
+        minutes = 0;
+        hours++
+    }
+    let format = (hours < 10 ? '0'+ hours : hours) + ":" + (minutes < 10 ? '0'+ minutes : minutes) + ":" + (seconds < 10 ? '0'+ seconds : seconds)
+    document.getElementById('timer').innerHTML = format
 }
